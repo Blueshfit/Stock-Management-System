6818bd7b5acc303c63dff69d32ebeb53ddb57bfa
ayon749 <mushfiqurrahman749@gmail.com>
2019-11-05 21:55:13 +0600

    catagory class added

---
+++ Stock Management System/Stock Management System/Models/Catagory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stock_Management_System.Models
+{
+	public class Catagory
+	{
+		public Catagory(string catagoryName)
+		{
+			CatagoryName = catagoryName;
+		}
+
+		public Catagory(int catagoryId, string catagoryName)
+		{
+			CatagoryId = catagoryId;
+			CatagoryName = catagoryName;
+		}
+
+		public int CatagoryId { set; get; }
+		public string CatagoryName { set; get; }
+	}
+}
